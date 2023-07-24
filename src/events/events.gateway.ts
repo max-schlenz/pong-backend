aaca833cc5c3d848860de536e922271cc604d422
max-schlenz <95466730+max-schlenz@users.noreply.github.com>
2023-07-24 19:25:40 +0000

    pad ball properly sent

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -1,9 +1,37 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Server } from 'socket.io';
+
+let paddlePos = 0;
+let ballPos = {x: 0, y: 0};
 
 @WebSocketGateway()
 export class EventsGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
-  }
+
+
+    @WebSocketServer() 
+    server: Server;
+
+    @SubscribeMessage('message')
+    handleMessage(client: any, payload: any): string {
+        return 'Hello world!';
+    }
+    
+    @SubscribeMessage('paddleY')
+    updatePaddleY(client: any, paddleY: number): void {
+      paddlePos = paddleY;
+      console.log("Paddle: ", paddleY);
+    }
+
+    @SubscribeMessage('ballX')
+    updateBallX(client: any, ballX: number): void {
+      ballPos.x = ballX;
+      console.log("X: ", ballPos.x);
+    }
+    
+    @SubscribeMessage('ballY')
+    updateBallY(client: any, ballY: number): void {
+      ballPos.y = ballY;
+      console.log("Y: ", ballPos.y);
+    }
+
 }
