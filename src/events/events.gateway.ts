c8cdb70ca4e98cd97086a40865fa76deb4fb61bb
max-schlenz <mschlenz@student.42heilbronn.de>
2023-07-25 13:34:55 +0000

    working on mp

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -1,37 +1,73 @@
-import { SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
-import { Server } from 'socket.io';
+import { SubscribeMessage, WebSocketGateway, OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
 
-let paddlePos = 0;
-let ballPos = {x: 0, y: 0};
+// let paddlePos = 0;
+
+// let isMovingUp = false;
+// let isMovingDown = false;
+// let ballPos = {x: 0, y: 0};
 
 @WebSocketGateway()
 export class EventsGateway {
 
-
-    @WebSocketServer() 
     server: Server;
-
+	
+	afterInit(server: Server) {
+		this.server = server;
+		console.log('Server is ready');
+	  }
+	  
+	  handleConnection(client: Socket, ...args: any[]) {
+		console.log(`Client connected: ${client.id}`);
+	  }
+	
+	  handleDisconnect(client: Socket) {
+		console.log(`Client disconnected: ${client.id}`);
+	  }
     @SubscribeMessage('message')
     handleMessage(client: any, payload: any): string {
         return 'Hello world!';
     }
-    
-    @SubscribeMessage('paddleY')
-    updatePaddleY(client: any, paddleY: number): void {
-      paddlePos = paddleY;
-      console.log("Paddle: ", paddleY);
-    }
 
-    @SubscribeMessage('ballX')
-    updateBallX(client: any, ballX: number): void {
-      ballPos.x = ballX;
-      console.log("X: ", ballPos.x);
-    }
-    
-    @SubscribeMessage('ballY')
-    updateBallY(client: any, ballY: number): void {
-      ballPos.y = ballY;
-      console.log("Y: ", ballPos.y);
-    }
+	@SubscribeMessage('paddleAMove')
+	handlePaddleAMove(client: any, newPosition: number): void {
+		client.broadcast.emit('paddleAMove', newPosition);
+		return;
+	}
 
+	@SubscribeMessage('paddleBMove')
+	handlePaddleBMove(client: any, newPosition: number): void {
+		client.broadcast.emit('paddleBMove', newPosition);
+		return;
+	}
 }
+    // @SubscribeMessage('moveUp')
+    // updateMoveUp(client: any, moveUp: boolean): void {
+	// 	client.emit('moveUp', true);
+	// 	client.emit('moveDown', false);
+    // //   console.log("Paddle: ", paddleY);
+    // }
+	
+    // @SubscribeMessage('moveDown')
+    // updateMoveDown(client: any, moveUp: boolean): void {
+	// 	client.emit('moveUp', false);
+	// 	client.emit('moveDown', true);
+    //   console.log("Paddle: ", paddleY);
+    // @SubscribeMessage('paddleY')
+    // updatePaddleY(client: any, paddleY: number): void {
+    //   paddlePos = paddleY;
+	//   client.emit('ballPosResponse', paddlePos);
+    // //   console.log("Paddle: ", paddleY);
+    // }
+
+    // @SubscribeMessage('ballX')
+    // updateBallX(client: any, ballX: number): void {
+    //   ballPos.x = ballX;
+    //   console.log("X: ", ballPos.x);
+    // }
+    
+    // @SubscribeMessage('ballY')
+    // updateBallY(client: any, ballY: number): void {
+    //   ballPos.y = ballY;
+    //   console.log("Y: ", ballPos.y);
+    // }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { IoAdapter } from '@nestjs/platform-socket.io';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+	const app = await NestFactory.create(AppModule);
+	const logger = new Logger('Bootstrap');
+	app.enableCors();
+	app.useWebSocketAdapter(new IoAdapter(app));
+	
+	try {
+	  await app.listen(3000, '0.0.0.0');
+	  logger.log('Nest application successfully started');
+	} catch (error) {
+	  logger.error('Error starting Nest application', error);
+	}
+  }
+  bootstrap();
